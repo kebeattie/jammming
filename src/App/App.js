a12ef0775ea10abd5c6db6d3151a75810fa623b8
kebeattie <kebeattie98@hotmail.com>
2024-05-14 14:46:02 +0100

    Added CSS modules

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
     </div>
   );
 }
