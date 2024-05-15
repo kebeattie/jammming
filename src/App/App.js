cd94d9c051c11030c441a4adbeefbd726aea4c53
kebeattie <kebeattie98@hotmail.com>
2024-05-15 14:42:39 +0100

    Added search feature

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,29 @@
 import styles from './App.module.css';
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 //import Track from '../Track/Track';
 import SearchResults from '../SearchResults/SearchResults';
 
 function App() {
 
-  //Fake search result track data to feed to track component until we connect to API 
+  //Track state of user input in search bar
+  const [userInput, setUserInput] = useState('');
+  //Track state of search results so we can re render search result component on search
+  const [searchResultsState, setSearchResultsState] = useState([]);
+
+  //Fake track data to feed to track component until we connect to API 
   const tracks = [
     {
       name: "Man Don't Care",
       album: "Integrity",
       artist: "JME",
-      id: 0
+      id: '0'
     },
     {
       name: "Gorilla",
       album: "No Thank You",
       artist: "Little Simz",
-      id: 1
+      id: '1'
 
     },
 
@@ -26,22 +31,58 @@ function App() {
       name: "Uptown Top Ranking",
       album: "Uptown Top Ranking",
       artist: "Althea & Donna",
-      id: 2
+      id: '2'
     },
 
-    
+    {
+      name: "How Much",
+      album: "Grime MC",
+      artist: "JME",
+      id: '3'
+    }
+
+  ];
+
+  const searchResults = []; //Array to store results from each search, will be set to searchResultsState
+  let searchForTracks = (searchString) => {
+    if (searchString.length > 1) {
+
+      tracks.forEach((track) => {
+        for (const key in track) {
+
+          let value = track[key].split(' ').join('').toLowerCase();
+          let userInput = searchString.split(' ').join('').toLowerCase();
+          if (value.includes(userInput)) {
+
+            searchResults.push(track);
+
+            break;
+
+          }
+        }
+      })
+
+    }
+    setSearchResultsState(searchResults);
 
 
-  ]
+
+  };
+
+
+
+  //Handler to track state change for user input
+  const handleUserInputChange = (input) => {
+    setUserInput(input);
+  }
 
 
   return (
     <div className='App'>
       <header><h1>Jammming</h1></header>
-      <SearchBar />
+      <SearchBar userInput={userInput} onChange={handleUserInputChange} searchForTracks={searchForTracks} tracks={searchResultsState} />
       <div className={styles.container}>
-        <SearchResults tracks={tracks} />
-        <SearchResults tracks={tracks} />
+        <SearchResults key={searchResultsState} tracks={searchResultsState} />
       </div>
 
     </div>
