080c52e8bd693dd56097a18517777800612328dc
kebeattie <kebeattie98@hotmail.com>
2024-05-16 15:15:26 +0100

    Connected to spotify API and can now search and fetch data

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import styles from './App.module.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 //import Track from '../Track/Track';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+
 
 function App() {
 
@@ -15,65 +16,95 @@ function App() {
   const [trackList, setTracklist] = useState([]);
   //Track state of playlsit name
   const [playlistName, setPlaylistName] = useState('')
-
-  //Fake track data to feed to track component until we connect to API 
-  const tracks = [
-    {
-      name: "Man Don't Care",
-      album: "Integrity",
-      artist: "JME",
-      uri: '0'
-    },
-    {
-      name: "Gorilla",
-      album: "No Thank You",
-      artist: "Little Simz",
-      uri: '1'
-
-    },
-
-    {
-      name: "Uptown Top Ranking",
-      album: "Uptown Top Ranking",
-      artist: "Althea & Donna",
-      uri: '2'
-    },
-
-    {
-      name: "How Much",
-      album: "Grime MC",
-      artist: "JME",
-      uri: '3'
-    }
-
-  ];
+  //Track access token
+  const [accessToken, setAccessToken] = useState('');
+  //Track spotify results
+  const [spotifyResults, setSpotifyResults] = useState({});
 
 
-  const searchResults = []; //Array to store results from each search, will be set to searchResultsState
-  let searchForTracks = (searchString) => {
-    if (searchString.length > 1) {
+  //Use this to get API token, will update so it only calls after current token expires
+  useEffect(() => {
+    const clientId = 'eb8ca22582be4f4b902ced6061756e1a';
+    const clientSecret = '957ed936baf148c580b4ae07f403ddbf';
 
-      tracks.forEach((track) => {
-        for (const key in track) {
+    fetch("https://accounts.spotify.com/api/token", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
 
-          let value = track[key].split(' ').join('').toLowerCase();
-          let userInput = searchString.split(' ').join('').toLowerCase();
-          if (value.includes(userInput)) {
+      },
+      body: 'grant_type=client_credentials'
 
-            searchResults.push(track);
-
-            break;
-
-          }
-        }
-      })
-
-    }
-    setSearchResultsState(searchResults);
+    }).then(result => result.json())
+      .then(data => setAccessToken(data.access_token));;
+  }, [])
 
 
 
+
+
+  //Search request
+  async function search() {
+    const endpoint = `https://api.spotify.com/v1/search?q=${userInput}&type=track`
+
+    fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }).then(response => response.json())
+      .then(data => setSpotifyResults(data.tracks));
   };
+
+
+
+  let j = 0;
+  const searchResults = []; //Array to store results from each search, will be set to searchResultsState
+  const constructSearchResults = (spotifyResults) => {
+    for (const key in spotifyResults.items) {
+      if (j === 1) {
+        break;
+      }
+
+
+      for (let i = 0; i < 10; i++) {
+
+        //console.log(spotifyResults.items[i].name); //this gets us track name
+        //console.log(spotifyResults.items[i].artists[0].name); //this gets us artist name
+        //console.log(spotifyResults.items[i].album.name); //this gets us album name
+        //console.log(spotifyResults.items[i].uri); //this gets us uri
+        //console.log(spotifyResults.items[i]);
+
+
+        searchResults.push({
+          name: spotifyResults.items[i].name,
+          album: spotifyResults.items[i].album.name,
+          artist: spotifyResults.items[i].artists[0].name,
+          uri: spotifyResults.items[i].uri
+        })
+
+
+      }
+      j++
+    }
+    console.log(searchResults);
+  }
+
+
+
+
+  let searchForTracks = () => {
+    if (userInput.length > 0) {
+    search();
+    constructSearchResults(spotifyResults);
+    setSearchResultsState(searchResults);
+    }
+    
+  };
+
+
 
 
   //Event handlers for updating states
@@ -81,37 +112,42 @@ function App() {
   //Handler to track state change for user input
   const handleUserInputChange = (input) => {
     setUserInput(input);
+
+
   };
   //Handler to change state of tracklist on addition of track
   const addTrackHandler = (addTrack) => {
     //Check if the selected track is already in the tracklist
-    if(!JSON.stringify(trackList).includes(JSON.stringify(addTrack))) {
+    if (!JSON.stringify(trackList).includes(JSON.stringify(addTrack))) {
       setTracklist([...trackList, addTrack]);
     }
     else {
       alert('This track is already in the current playlist.')
-    } 
+    }
   };
   //Handler to change state of tracklist on removal of track
   const removeTrackHandler = (removeTrack) => {
-    const newList = trackList.filter((track) => JSON.stringify(track)!== JSON.stringify(removeTrack));
+    const newList = trackList.filter((track) => JSON.stringify(track) !== JSON.stringify(removeTrack));
     console.log(newList);
 
-    setTracklist(newList); 
+    setTracklist(newList);
   };
   //Handler to change state of playlist name
   const handlePlaylistName = (input) => {
     setPlaylistName(input);
   };
-  //Handler to export playlist to sporift (fake data for now)
+  //Handler to export playlist to spotify (fake data for now)
   const onSaveHandler = () => {
     let uriArray = [];
     trackList.forEach((track) => {
       uriArray.push(track.uri);
     })
-    const exportPlaylist =[playlistName, uriArray];
+    const exportPlaylist = [playlistName, uriArray];
     console.log(exportPlaylist);
-  }; 
+
+    setTracklist([]);
+    setPlaylistName('');
+  };
 
 
 
@@ -122,7 +158,7 @@ function App() {
       <SearchBar userInput={userInput} onChange={handleUserInputChange} searchForTracks={searchForTracks} tracks={searchResultsState} />
       <div className={styles.container}>
         <SearchResults key={searchResultsState} tracks={searchResultsState} trackList={trackList} addTrackHandler={addTrackHandler} />
-        <Playlist tracks={trackList} removeTrackHandler={removeTrackHandler} handlePlaylistName={handlePlaylistName} onSaveHandler={onSaveHandler}/>
+        <Playlist tracks={trackList} removeTrackHandler={removeTrackHandler} handlePlaylistName={handlePlaylistName} onSaveHandler={onSaveHandler} />
       </div>
 
     </div>
